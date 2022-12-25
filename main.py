e6a893f5db5724f9010fada69883295b0222448c
Giovanni Cutrì <89142084+giovanni-cutri@users.noreply.github.com>
2022-12-25 16:44:20 +0100

    Added support for other countries

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
+import argparse
+from argparse import RawTextHelpFormatter
 import requests
 import bs4
+import sys
 import urllib.request
 import os
 
 domain_name = "https://inducks.org/"
 
-res = requests.get("https://inducks.org/country.php?c=it")
+res = requests.get("https://inducks.org/legend-country.php")
+res.raise_for_status()
+soup = bs4.BeautifulSoup(res.text, features="lxml")
+country_list = soup.select(".countryList a")
+
+countries = []
+country_codes = []
+
+with open("resources/country-codes.txt", "a") as f:
+    for i in country_list:
+        country = i.getText()
+        countries.append(country)
+        country_code = i.attrs["href"].split("?c=")[-1]
+        country_codes.append(country_code)
+        f.write("{:>20}   {:>2}".format(country, country_code) + "\n")
+
+parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)  # formatter needed for newline in help message
+parser.add_argument("country", help="the country of the publications whose covers you want to download\n" +
+                                    "For a list of valid country codes, see resources/country-codes.txt")
+args = parser.parse_args()
+country = args.country
+
+if country not in country_codes:
+    print("Error: could not find any publications\nPlease provide a valid country code")
+    sys.exit()
+
+res = requests.get("https://inducks.org/country.php?c=" + country)
 res.raise_for_status()
 soup = bs4.BeautifulSoup(res.text, features="lxml")
 
@@ -39,8 +68,8 @@ for i in link_testate:
         cover_elem = soup.select("figcaption a")
         if cover_elem:
             cover_url = domain_name + cover_elem[0].attrs["href"]
-            directory = "copertine/" + nomi_testate[counter]
-            path = "copertine/" + nomi_testate[counter] + "/" + issue_number + ".jpg"
+            directory = "covers/" + nomi_testate[counter]
+            path = "covers/" + nomi_testate[counter] + "/" + issue_number + ".jpg"
             if not os.path.exists(directory):
                 os.makedirs(directory)
             if not os.path.exists(path):
